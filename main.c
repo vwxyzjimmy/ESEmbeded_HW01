7513adf138338a08fb35f54d2568cf5f0472a3b5
vwxyzjimmy <vwxyzjimmy@gmail.com>
2019-02-15 18:44:47 +0800

    modified main.c to +-*/

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-#include <stdio.h>
-//123456789
-int main()
-{
-	printf("123456Hello World\n");
+#include<stdio.h>
+#include<stdlib.h>
+
+int plus(int a, int b){ return a+b; }
+int minus(int a, int b){ return a-b; }
+int multiply(int a, int b){ return a*b; }
+int divided(int a, int b){ return a/b; }
+int main(){
+	int a, c;
+	char b;
+	scanf("%i %c %i", &a, &b, &c);
+	//IF ELSE IF
+	if(b == '+')
+		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
+	else if(b == '-')
+		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
+	else if(b == '*')
+		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
+	else if(b == '/')
+		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
 }
