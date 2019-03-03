b90eac69bfa332f3799d9985e2814ff78fb79bd5
vwxyzjimmy <vwxyzjimmy@gmail.com>
2019-03-03 14:56:31 +0800

    modified main.c

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ int main(){
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
 	//IF ELSE IF
+	/*
 	if(b == '+')
 		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
 	else if(b == '-')
@@ -19,4 +20,12 @@ int main(){
 		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
 	else if(b == '/')
 		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	*/
+	//ARRAY OF FUNCTION POINTER
+	char operator[4] = {'+', '-', '*', '/'};
+	int(*operation[4])(int, int) = {plus, minus, multiply, divided};
+	for(int i=0;i<4;i++){
+		if (b == operator[i])
+			printf("%d %c %d = %d\n", a, b, c, (*operation[i])(a, c));
+	}
 }
--- readme.md
+++ readme.md
@@ -19,4 +19,3 @@ This is the hw01 sample. Please follow the steps below.
 # HW01 Requirements
 1. We are going to practice some C skills about function pointers and arrays of function pointers, please modify main.c with arrays of function pointers to simplified the code.
 2. You have to push your code to your github. (Using .gitignore to exclude the outpufiles like object files or executable files)
-3. Make sure hand in your github account on moodle.
